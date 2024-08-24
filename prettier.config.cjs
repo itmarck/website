677c67626bd5894169142b148ef8c7bd1dbd236f
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2024-08-24 17:28:19 -0500

    Resume template

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	// i am just using the standard config, change if you need something else
+	...require('prettier-config-standard'),
+	pluginSearchDirs: [__dirname],
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro'
+			}
+		}
+	],
+	useTabs: false,
+	printWidth: 80,
+	singleQuote: true,
+	trailingComma: 'all'
+}
